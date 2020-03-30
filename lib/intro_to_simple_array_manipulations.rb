5431e851d3b6cecb3e40b7046044df6ffa1ba9f1
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-30 21:53:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,6 +14,10 @@ def using_flatten(array)
   array.flatten()
 end
 
-def using_delete_at(array, string)
-  array.delete_at(string)
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
 end
